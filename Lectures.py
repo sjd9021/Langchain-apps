513a5c05c3411e15adfda2414b6e8c90e75f6428
sjd9021 <samvit@hotmail.com>
2023-07-12 16:59:39 +0530

    Test2

---
+++ Lectures.py
@@ -0,0 +1,86 @@
+import openai
+import os
+from dotenv import load_dotenv
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain.llms import AzureOpenAI
+from langchain.chat_models import AzureChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_DEPLOYMENT_ENDPOINT = os.getenv("OPENAI_DEPLOYMENT_ENDPOINT")
+OPENAI_DEPLOYMENT_NAME = os.getenv("OPENAI_DEPLOYMENT_NAME")
+OPENAI_MODEL_NAME = os.getenv("OPENAI_MODEL_NAME")
+OPENAI_EMBEDDING_DEPLOYMENT_NAME = os.getenv("OPENAI_EMBEDDING_DEPLOYMENT_NAME")
+OPENAI_EMBEDDING_MODEL_NAME = os.getenv("OPENAI_EMBEDDING_MODEL_NAME")
+OPENAI_DEPLOYMENT_VERSION = os.getenv("OPENAI_DEPLOYMENT_VERSION")
+
+#init Azure OpenAI
+openai.api_type = "azure"
+openai.api_version = OPENAI_DEPLOYMENT_VERSION
+openai.api_base = OPENAI_DEPLOYMENT_ENDPOINT
+openai.api_key = OPENAI_API_KEY
+
+pdfs = os.scandir('transcript')
+loaders = []
+for i in pdfs:
+    loaders.append(PyPDFLoader(i.path))
+
+docs = []
+for loader in loaders:
+    docs.extend(loader.load())
+    
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 100
+)
+splits = text_splitter.split_documents(docs)
+
+
+embedding=OpenAIEmbeddings(deployment=OPENAI_EMBEDDING_DEPLOYMENT_NAME,model=OPENAI_EMBEDDING_MODEL_NAME, chunk_size=1)
+vectordb = FAISS.from_documents(
+    documents=splits,
+    embedding=embedding
+)
+
+vectordb.save_local("dbs/documentation/faiss_index")
+
+llm = AzureChatOpenAI(    
+                  deployment_name=OPENAI_DEPLOYMENT_NAME,
+                      model=OPENAI_MODEL_NAME,
+                      openai_api_base=OPENAI_DEPLOYMENT_ENDPOINT,
+                      openai_api_version=OPENAI_DEPLOYMENT_VERSION,
+                      openai_api_key=OPENAI_API_KEY
+                     )
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever()
+)
+template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
+{context}
+Question: {question}
+Helpful Answer:"""
+QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+from langchain.memory import ConversationBufferMemory
+memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    return_messages=True
+)
+from langchain.chains import ConversationalRetrievalChain
+retriever=vectordb.as_retriever()
+qa = ConversationalRetrievalChain.from_llm(
+    llm,
+    retriever=retriever,
+    memory=memory,
+    combine_docs_chain_kwargs={"prompt": QA_CHAIN_PROMPT}
+)
+question = "Give me a summary of lecture 1"
+result = qa.run(question)
+print(result)
